582c9b80794b8de451b08d00ed59c812b2d5280a
allburov <allburov@gmail.com>
2023-05-08 18:35:34 +0700

    Add Patron structure

--- internal/patrons/patrons.go
+++ internal/patrons/patrons.go
@@ -1,1 +1,13 @@
 package patrons
+
+import "time"
+
+type Patron struct {
+	Platform   string
+	Level      string
+	Name       string
+	Email      string
+	Status     string
+	ActiveTill time.Time
+	Perks      map[string]interface{}
+}
